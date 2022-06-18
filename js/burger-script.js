c0fc5a0f67e73129ed066aa92fa91282c6857cdc
Andrey Zaderenko <andrey42work@gmail.com>
2022-06-18 17:26:21 +0300

    fix: some fixes

--- js/burger-script.js
+++ js/burger-script.js
@@ -2,8 +2,18 @@
 
 let menuBurger = document.querySelector('.menu_burger');
 let burgerUl = document.querySelector('.burger_ul');
+let linkClose = document.querySelectorAll('.nav_link');
+console.log(linkClose);
 
 menuBurger.addEventListener('click', function(){
 	menuBurger.classList.toggle('active');
 	burgerUl.classList.toggle('active');
 });
+
+    linkClose.forEach(navLink => {
+        navLink.addEventListener('click', function() {
+            if (menuBurger.classList.contains('active')) {
+            menuBurger.classList.toggle('active');
+	        burgerUl.classList.toggle('active');}
+        });
+    });
